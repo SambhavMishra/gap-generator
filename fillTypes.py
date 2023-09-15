1774a80b1724222c331e5187595e7c1c10d1c673
SambhavMishra <sambhavmishra2512@gmail.com>
2023-09-15 16:39:25 +0530

    improved spaces

--- fillTypes.py
+++ fillTypes.py
@@ -169,15 +169,15 @@ class fillTypes:
             contents = file_.read()
             data_url = base64.b64encode(contents).decode("utf-8")
             file_.close()
-
-            markers = f'<img src="data:image/gif;base64,{data_url}"  alt="omr marker" width="80px" style="display:inline;"/> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <img src="data:image/gif;base64,{data_url}"  alt="omr marker" width="80px" style="display: inline;"/> <br/><br/>'
+            space = "&nbsp;"
+            markers = f'<img src="data:image/gif;base64,{data_url}"  alt="omr marker" width="80px" style="display:inline;"/> {space*numBoxes*11}<img src="data:image/gif;base64,{data_url}"  alt="omr marker" width="80px" style="display: inline;"/> <br/><br/>'
 
             res += markers
 
-            res += "Roll No.: " + f'<span style="font-size: 24px; border: 1px solid black; display: inline;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>'*2 + " <br/>"
+            res += f"{space*16}Roll No.: " + f'<span style="font-size: 24px; border: 1px solid black; display: inline;">{space*6}</span>'*2 + " <br/>"
 
             for char in charList:
-                res += f'<br/> {char} &nbsp;&nbsp;&nbsp;&nbsp;' +  f'<span style="font-size: 24px; border: 1px solid black; display: inline;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>'*numBoxes 
+                res += f'<br/> {space*16} {char} &nbsp;&nbsp;&nbsp;&nbsp;' +  f'<span style="font-size: 24px; border: 1px solid black; display: inline;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>'*numBoxes 
 
             res += '<br/><br/>' + markers
 
